f1503ad6e3680958996c1ae9283c772e1a1e12a8
Samuel Romero <samuel72340@gmail.com>
2023-05-19 17:38:39 -0500

    Act

--- Control_De_Flujo/controlDeflujo/Program.cs
+++ Control_De_Flujo/controlDeflujo/Program.cs
@@ -6,9 +6,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        //if_();
+        if_();
 
-        Switch();
+        switch_();
+
+        while_();
+
+        Ejer1();
     }
 
     static void if_(){
@@ -25,7 +29,7 @@ class Program
         Console.WriteLine("Tienes Carnet");
         carnet=Console.ReadLine();
 
-        if (carnet=="si" && num1>=num2 && num1<=60){              //EL "if" solo jecuta lo que esta dentro de si, si la condicion es verdadera
+        if (carnet=="si" && num1>=num2 && num1<=60){              //EL "if" solo ejecuta lo que esta dentro de si, si la condicion es verdadera
             Console.WriteLine("Puedes conducir vehiculos=");    
             Console.WriteLine(bool1);
         }
@@ -38,7 +42,7 @@ class Program
 
     }
 
-    static void Switch(){
+    static void switch_(){
         string Direccion;
 
         Console.WriteLine("Programa de encuesta");
@@ -70,5 +74,71 @@ class Program
                 Console.WriteLine("Asegurate de estar escribiendo bien la respuesta");
                 break;
         }
+    }
+
+    static void while_(){               
+        //esta el bucle "while" que ejecutara lo de su interior si la condición se cumple y el bucle "do while" qu eejecuta su interior 1 vez aunque la condición sea falsa
+        //la sintaxis es:   do{Console.WriteLine("Hola Mundo");}while(3!=4);
+
+        string resp1;
+        int resu1;
+        int num1;
+        int num2;
+        int num3;
+        
+        Console.WriteLine("Bienvenido al programa de sumas");
+        Console.WriteLine("Introduce el primer numero:");
+        num1=int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Introduce el siguiente numero:");
+        num2=int.Parse(Console.ReadLine());
+
+        resu1=num1+num2;
+
+        Console.WriteLine("¿Quieres sumar otro numero?");
+        resp1=Console.ReadLine();
+
+        while(resp1!="no"){                     //El while repite lo de su interior simpre que la condición se cumpla
+            Console.WriteLine("Introduce el siguiente numero:");
+            num3=int.Parse(Console.ReadLine());
+            resu1=resu1+num3;
+            
+            Console.WriteLine("¿Quieres sumar otro numero?");
+            resp1=Console.ReadLine();
+        }
+        
+        Console.WriteLine("El resultado es:"+resu1);
+        Console.WriteLine("Gracias por usar el programa");
+    }
+
+    static void Ejer1(){
+        Random numero=new Random();             //Estas dos lineas de codigo me permiten generar un numero aleatorio
+        int numAleatorio=numero.Next(0,100);
+
+        int res1;
+        int numIntentos=0;
+
+        Console.WriteLine("Bienvenido al juego");
+        Console.WriteLine("Tienes que introdicir un numero del 0 al 100 hasta adivinar el correcto, el programa va a dar pistas");
+        Console.WriteLine("Empezemos. Introduce un numero del 0 al 100:");
+        res1=int.Parse(Console.ReadLine());
+        numIntentos++;
+
+        while(res1 != numAleatorio){
+            
+            if (res1<numAleatorio){
+                Console.WriteLine("El numero oculto es mayor");
+            }
+            else{
+                Console.WriteLine("El numero oculto es menor");
+            }
+            Console.WriteLine("Intentalo de nuevo:");
+            res1=int.Parse(Console.ReadLine());
+
+            numIntentos++;
+        }
+
+        Console.WriteLine($"Felicidades, Ganaste en {numIntentos} intentos");
+
     }
 }
---
+++ POO/POO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace POO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+    }
+}
